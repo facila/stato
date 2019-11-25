9007e9126b9d12a54f3fdc07af6dba25f32f0412
facila <51124100+facila@users.noreply.github.com>
2019-11-25 17:36:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,6 @@
 [ "`whoami`" != 'root' ] && { echo vous devez être root pour exécuter install.sh ; exit ; }
 FILE=$1 ; [ ! -f $FILE ] && { echo "le fichier $FILE n'existe pas" ; exit ; }
 
-FACILA=/usr/local/facila
- DROIT=$FACILA/share/prg/sys_droit.sh
-if [ ! -d $FACILA ]
-then mkdir $FACILA $FACILA/share $FACILA/share/prg
-     tail -17 install.sh > $DROIT
-     chmod 744 $DROIT
-fi
-
 APPLI=`echo $FILE | cut -f1 -d'.'`
 
 echo vérification des dépendances de $APPLI
@@ -36,28 +28,3 @@ cp $FILE /$FILE
 cd /
 tar -xzf $FILE
 rm $FILE
-
-echo configuration des droits de facila
-$DROIT
-
-exit
-
-# facila/share/prg/sys_droit.sh créé à la première installation par la commande tail de install.sh
-
-#!/bin/bash
-
-cd /usr/local
-chown -R root:root facila
-chmod -R 755       facila
-
-cd facila
-chmod 744 share/prg/sys_droit.sh
-
-find . -type d -name var -exec chmod -R 644 {} \;
-find . -type d -name var -exec chmod -R u+X,g+X,o+X {} \;
-
-find . -type d -name tmp -exec chmod -R 666 {} \;
-find . -type d -name tmp -exec chmod -R u+X,g+X,o+X {} \;
-
-find . -type d -name data -exec chmod -R 666 {} \;
-find . -type d -name data -exec chmod -R u+X,g+X,o+X {} \;
