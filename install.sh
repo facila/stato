1260c420b58dc06bb2ffeb12cf0f993ae3fe764e
facila <51124100+facila@users.noreply.github.com>
2020-11-22 17:25:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# l'installation se fait en root
 # se positionner dans le répertoire contenant install.sh et le fichier .tar.gz
-# passer la commande : sh install.sh
+# exécuter la commande : sudo sh install.sh
 
 APPLI=stato
 VERSION=2.02
 
-[ "`whoami`" != 'root' ] && { echo vous devez être root pour exécuter install.sh ; exit ; }
+[ "`whoami`" != 'root' ] && { echo vous devez exécuter : sudo sh install.sh ; exit ; }
 
 # vérification des dépendances
-ERROR=''
-[ "`perl -v`"                       = '' ] && ERROR=$ERROR"perl   "
-[ "`perl -e 'use Tk' 2>/dev/null`" != '' ] && ERROR=$ERROR"perl-tk   "
-[ "$ERROR" != '' ] && { echo "vous devez d'abbord installer : $ERROR" ; exit ; }
+perl -e ''       2>/dev/null ; [ $? != "0" ] && { echo "vous devez d'abbord installer : perl"    ; exit ; }
+perl -e 'use Tk' 2>/dev/null ; [ $? != "0" ] && { echo "vous devez d'abbord installer : perl-tk" ; exit ; }
 
 FILE=$APPLI.$VERSION.tar.gz
 echo installation de facila $FILE
