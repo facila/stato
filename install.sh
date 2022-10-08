b19f2c94ab267f3fc7a1705a45da589f4d24d9e4
facila <facila@gmx.fr>
2022-10-08 15:04:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# version 1.02 Juillet 2021
+# version 2.00 octobre 2022
 # se positionner dans le répertoire contenant install.sh et le FICHIER tar.gz à installer
-# exécuter la commande : sudo sh install.sh FICHIER
+# exécuter la commande : install.sh FICHIER
+
+proc_exit ()
+{
+perl -e "$1" 2>/dev/null
+[ $? != "0" ] && { echo "vous devez d'abbord installer : $2" ; exit ; }
+}
 
 proc_appli ()
 {
@@ -13,37 +19,71 @@ esac
 
 proc_perl ()
 {
-proc_exit ''       perl 
+proc_exit ''       perl
 proc_exit 'use Tk' perl-tk
 }
 
-proc_exit ()
+proc_facila ()
 {
-perl -e "$1" 2>/dev/null
-[ $? != "0" ] && { echo "vous devez d'abbord installer : $2" ; exit ; }
+OK=1
+if [ "$FACILA" = "" ]
+then FACILA="$DIR/facila"
+     printf "\n# FACILA\nexport FACILA=$FACILA\n" >> ~/.bashrc
+     OK=0
+fi
+
+[ ! -d $FACILA ] && mkdir facila facila/old facila/archive facila/version
+cd $FACILA
 }
 
-##########################################################################
+proc_old ()
+{
+[ ! -f "$DIR/install_$APPLI" ] && return
+cat $DIR/install_$APPLI | while read OLD
+do mkdir -p old/$OLD # création des répertoires contenus dans $OLD
+   rmdir    old/$OLD # suppression du dernier repertoire de $OLD
+   mv $FACILA/$OLD old/$OLD.`date +%y%m%d_%H%M` 2> /dev/null
+done
+}
 
-FILE=$1
+proc_lang ()
+{
+# copie du répertoire d'origine $LG dans la langue de la machine $LANG
+[ "$LG" = "$LANG" ] && return
+cp -R $FACILA/$APPLI/var/$LG $FACILA/$APPLI/var/$LANG
+echo "votre langue est $LANG"
+echo "vous pouvez traduire les fichiers ( menu , aide , ... )"
+}
+
+proc_end ()
+{
+if [ "$OK" = "1" ]
+then echo "vous pouvez exécuter $APPLI"
+else echo "fermer et relancer le shell pour exécuter $APPLI"
+fi
+echo "commande : $FACILA/$APPLI/prg/$APPLI"
+}
+
+#################################################################################
+
+ FILE=$1
 APPLI=`echo $FILE | cut -f1 -d.`
   EXT=`echo $FILE | cut -f4-5 -d.`
+  DIR=$PWD
+   LG=fr_FR.UTF-8
 
-DIR=/usr/local/facila
-LG=fr_FR.UTF-8
+[ "$EXT" != "tar.gz" ] && { echo le fichier $FILE doit être un tar.gz ; exit ; }
+[ ! -s "$FILE"       ] && { echo fichier $FILE absent ; exit ; }
 
-[ "`whoami`" != 'root'   ] && { echo vous devez exécuter : sudo sh install.sh FICHIER ; exit ; }
-[ "$EXT"     != "tar.gz" ] && { echo le fichier $FILE doit être un tar.gz ; exit ; }
-[ ! -s $FILE             ] && { echo fichier $FILE absent ; exit ; }
+[ "`dirname $FILE`" = "." ] && FILE="$DIR/$FILE"
 
-# vérification des dépendances
-proc_appli
+echo vérification des dépendances
 proc_perl
+proc_appli
 
-# installation
 echo installation de facila $FILE
-tar -xzf $FILE -C /
-
-# copie du répertoire d'origine $LG dans la langue de votre machine $LANG
-# vous pouvez-ensuite traduire les fichiers ( menu , aide , ... )
-[ "$LG" != "$LANG" ] && cp -R $DIR/$APPLI/var/$LG $DIR/$APPLI/var/$LANG
+proc_facila
+proc_old
+tar -xzf $FILE
+proc_lang
+proc_end
